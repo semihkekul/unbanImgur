472a98d13df8fda3a9cf351b39ff29eb3eeb753b
semih.kekul <semih.kekul@arcelik.com>
2018-02-22 18:45:35 +0300

    bazen duckduckgo hata veriyor..

--- background.js
+++ background.js
@@ -1,13 +1,20 @@
-var host = "https://images.duckduckgo.com/iu/?u=https://i.imgur.com";
+//var host = "https://images.duckduckgo.com/iu/?u=https://i.imgur.com";
+var host  = "https://proxy.duckduckgo.com/iu/?u="
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]+".jpg"};
+         urlArray = details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/);
+         var res = urlArray[0].split(":");
+         return {redirectUrl: host + urlArray[0]};
+        // return chrome.tabs.create({ url: host + urlArray[0] });
     },
     {
         urls: [
-            "*://imgur.com/*",
-            "*://www.imgur.com/*",
-            "*://i.imgur.com/*",
+            "https://imgur.com/*",
+            "http://imgur.com/*",
+            "https://www.imgur.com/*",
+            "http://www.imgur.com/*",
+            "http://i.imgur.com/*",
+            "https://i.imgur.com/*",
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
