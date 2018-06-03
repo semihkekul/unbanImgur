620894c5769ae58dbe2df6cb2aa6c5a6327e98d6
Semih Kekül <semihkekul@gmail.com>
2018-06-03 15:03:12 +0300

    icon düzenlemesi

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-//var host = "https://images.duckduckgo.com/iu/?u=https://i.imgur.com";
+
 var host  = "https://proxy.duckduckgo.com/iu/?u="
+// find if it is an imgur image aweb page and redirect using duckduck go
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
          urlArray = details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/);
          var res = urlArray[0].split(":");
+         console.log(urlArray[0])
          return {redirectUrl: host + urlArray[0]};
-        // return chrome.tabs.create({ url: host + urlArray[0] });
+       
     },
     {
         urls: [
